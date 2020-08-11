54b7cc92a0b4eea8493db87ca95a531429e9cf2c
Arian471 <Arian471@gmail.com>
2020-08-11 14:55:37 +0200

    Tilføjet Cipher Decipher test

--- test/test.js
+++ test/test.js
@@ -1,12 +1,10 @@
 const fc = require('fast-check');
 fc.configureGlobal({numRuns: 100});
 const util = require ('util');
-
-// Code under test
-const contains = (text, pattern) => text.indexOf(pattern) >= 0;
+const crypto = require('crypto');
 
 
-describe('util', () => {
+describe('util encode & decode', () => {
     const encoder = new TextEncoder();
     const decoder = new TextDecoder();
 
@@ -14,21 +12,46 @@ describe('util', () => {
         fc.assert(fc.property(fc.string(), text => decoder.decode(encoder.encode(text)) === text),{verbose: true})
     });
     it('when encoded it should not contain the original', () => {
-        fc.assert(fc.property(fc.string(), text => !contains(encoder.encode(text), text)),{verbose: true})
+        fc.assert(fc.property(fc.string(), text => !encoder.encode(text).includes(text)),{verbose: true})
     });
 });
 
-// Properties
-describe('properties', () => {
-    // string text always contains itself
-    it('should always contain itself', () => {
-        fc.assert(fc.property(fc.string(), text => contains(text, text)));
-    });
-    // string a + b + c always contains b, whatever the values of a, b and c
-    it('should always contain its substrings', () => {
-        fc.assert(fc.property(fc.string(), fc.string(), fc.string(), (a,b,c) => {
-            // Alternatively: no return statement and direct usage of expect or assert
-            return contains(a+b+c, b);
-        }));
-    });
+
+describe('Crypto cipher & decipher', () => {
+    const algorithm = 'aes-192-cbc';
+
+    it('when ciphered and deciphered it should match the original', () => {
+        fc.assert(fc.property(fc.string(), fc.string(), (message, password) => {
+            const key = crypto.scryptSync(password, 'salt', 24)
+            const iv = Buffer.alloc(16, 0); // todo: make randon and not static
+            const cipher = crypto.createCipheriv(algorithm, key, iv);
+            const decipher = crypto.createDecipheriv(algorithm, key, iv);
+            let encrypted = '';
+            cipher.on('readable', () => {
+                let chunk;
+                while (null !== (chunk = cipher.read())) {
+                    encrypted += chunk.toString('hex');
+                }
+            });
+            cipher.on('end', () => {
+                decipher.write(encrypted, 'hex');
+                decipher.end();
+                // Prints: e5f79c5915c02171eec6b212d5520d44480993d7d622a7c4c2da32f6efda0ffa
+            });
+
+            let decrypted = '';
+            decipher.on('readable', () => {
+                while (null !== (chunk = decipher.read())) {
+                    decrypted += chunk.toString('utf8');
+                }
+            });
+            decipher.on('end', () => {
+                return decrypted === message;
+            });
+
+            cipher.write(message);
+            cipher.end();
+
+        }),{verbose: true})
+    }).timeout(5000);
 });
