77e6804b6025a779f5b1cee6c03fca6d63e5229f
Jonas Mohr Pedersen <mohrdevelopment@gmail.com>
2020-08-12 16:32:27 +0200

    chunk test

--- test/lodashtest.js
+++ test/lodashtest.js
@@ -10,7 +10,7 @@ const getRandomIntInclusive = (min, max) => {
 
 describe('Array', () => {
     describe('chunk()', () => {
-        it('returns arrays of expected size', () => {
+        it('should return arrays of expected size', () => {
             fc.assert(
                 fc.property(
                     fc.array(fc.string(), 10, 100),
@@ -19,9 +19,42 @@ describe('Array', () => {
                         const chunkedArray = _.chunk(array, chunkSize)
 
                         const chunkedArraySizes = chunkedArray.map(chunk => chunk.length)
-                        console.log('asd', chunkedArraySizes)
-                        return chunkedArray[0].length === chunkSize
+
+                        for(let i = 0; i < chunkedArraySizes.length; i++){
+                          return chunkedArraySizes[i] % chunkSize === 0
+                        }
+                        
             }))
+        })
+
+        it('should return chunked arrays', () => {
+          fc.assert(
+            fc.property(
+              fc.array(fc.string(), 10, 100), array => {
+                const chunkSize = getRandomIntInclusive(1, array.length)
+                const chunkedArray = _.chunk(array, chunkSize)
+                return chunkedArray.length === Math.ceil(array.length / chunkSize)
+              }
+            )
+          )
+        })
+
+        it('should return the last chunk as remaining elements', () => {
+          fc.assert(
+            fc.property(
+              fc.array(fc.string(), 10, 100), array => {
+                const chunkSize = getRandomIntInclusive(1, array.length)
+                const chunkedArray = _.chunk(array, chunkSize)
+                const remainingElements = array.length % chunkSize 
+
+                if(remainingElements !== 0) {
+                  return chunkedArray[chunkedArray.length -1].length === remainingElements
+                } else {
+                  return chunkedArray[chunkedArray.length - 1].length === chunkSize
+                }
+              }
+            )
+          , {verbose: true})
         })
     })
 })
